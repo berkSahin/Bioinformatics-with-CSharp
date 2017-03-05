4921bb390225f707fd478f1348e9192a027d5085
berkSahin <kasatura_0618@hotmail.com>
2017-03-06 01:18:58 +0200

    Add Alphabet and Stats

---
+++ Bioinfarmatics.Library/Core/Interfaces/IStats.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bioinfarmatics.Library.Core.Interfaces
+{
+    interface IStats
+    {
+        ulong Lenght();
+        void Sknewness();
+        void CpG();
+        void ZCurved();
+        void OrUr();
+        void Frekans();
+    }
+}
---
+++ Bioinfarmatics.Library/Core/Sequence.cs
@@ -0,0 +1,38 @@
+﻿using Bioinfarmatics.Library.Core.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bioinfarmatics.Library.Core
+{
+    public class Sequence:SequenceBase
+    {
+        public Direction Direction;
+        public Alphabet Alphabet; 
+
+        public override string Content
+        {
+            get
+            {
+                return base.Content;
+            }
+
+            set
+            {
+                base.Content = value;
+                Alphabet = new Alphabet(value);
+                Direction = new Direction();
+                if (!Alphabet.IsDNA())
+                    Direction.Complement();
+            }
+        }
+
+        public Sequence(string title, string content)
+        {
+            Title = title;
+            Content = content;
+        }
+    }
+}
